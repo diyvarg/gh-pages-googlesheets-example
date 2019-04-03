375b7c323a6449d588c74f288029259c7228edab
DiY <diy.varg@gmail.com>
2019-04-03 16:45:25 +0300

    Code added

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 
 # Минимальный сайт 
 
-Сайт создан с помощью jekyll, с хостингом в github pages, с формой ввода, которая сохраняет данные в google sheets. 
+Страница с хостингом на github pages и формой ввода, которая сохраняет данные в [таблице google sheets](https://docs.google.com/spreadsheets/d/178agwdIps-k_cHemF6GoE6HJzQ6yBlgYqnQETbNURHk/edit#gid=0). 
 
---
+++ src/googlesheetsapp.js
@@ -0,0 +1,39 @@
+// в "редактор скриптов" в меню выбрать: 
+// - Опубликовать 
+// - Развернуть как веб-приложение 
+// - Как запускать приложение: От моего имени
+// - Кто имеет доступ к приложению: Все, включая анонимных пользователей
+// - Обновить и получить Текущий URL веб-приложения:   
+var SHEET_URL = "https://docs.google.com/spreadsheets/d/178agwdIps-k_cHemF6GoE6HJzQ6yBlgYqnQETbNURHk/edit#gid=0";
+    
+function doGet(e) {
+    var lock = LockService.getPublicLock(); 
+    lock.waitLock(30000);  // 30 seconds
+    var result = null;
+    try {
+        var ssa = SpreadsheetApp.openByUrl(SHEET_URL);
+        var sheet = ssa.getSheets()[0]
+        var nextRow = setValues(sheet, e);
+        result = {"result" : "success", "row" : nextRow};
+    } catch(e) {
+        result = {"result" : "error", "error" : e};
+    }
+    lock.releaseLock();
+    return ContentService.createTextOutput(JSON.stringify(result))
+                         .setMimeType(ContentService.MimeType.JSON);
+}
+
+function setValues(sheet, e) {
+  var headers = sheet.getRange(1, 1, 1, sheet.getLastColumn())     // row, column, rows, columns 
+                     .getValues()[0];
+  var newrow = []; 
+  for (i in headers) {
+    newrow.push(e.parameter[headers[i]]);
+  }
+  var nextRow = sheet.getLastRow() + 1;      
+  sheet.getRange(nextRow, 1, 1, row.length)
+       .setValues([row]);
+  return nextRow;     
+}
+
+
